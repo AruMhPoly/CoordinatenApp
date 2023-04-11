40d3b82a4d3c78d1b13522053cd979816094aa70
AruMhPoly <127189008+AruMhPoly@users.noreply.github.com>
2023-04-11 11:49:51 +0200

    First codes

---
+++ Coordinaten.py
@@ -0,0 +1,105 @@
+﻿#In[]
+
+from arcgis.gis import GIS
+import pandas as pd
+import os 
+
+#In[]: 
+
+class ArcGIS:
+    def __init__(self,project_name='Project Mh Poly'):
+
+        self.project_name = project_name
+
+    def Results(self):
+
+        # Connect to your ArcGIS Online organization
+        gis = #Deleted due to privacy reasons
+
+        # Search for the feature layer by its name
+        feature_layer_name = self.project_name
+        results = gis.content.search(query=feature_layer_name, item_type="Feature Layer")
+
+        global LayerName
+        Names = [x.name for x in results]
+        for i in range(len(Names)):
+            if Names[i] == 'Boringen_HBR':
+                NP = results[i].id
+                LayerName = "Boringen_HBR"
+                return NP
+                break
+            if Names[i] == '22131N1-TE02 WBO IJmuiden Uitvoering WF':
+                NP = results[i].id
+                LayerName = "Boringen_IJmuiden"
+                return NP
+                break
+
+        NP = results[0].id
+        item = gis.content.get(NP)
+        LayerName = item.name
+        return NP
+
+            
+    def get_coordinates(self,NP):
+
+                
+        # Connect to your ArcGIS Online organization
+        gis = #Deleted due to privacy reasons
+
+        layer_item = gis.content.get(NP)
+        point_layer = None
+        feature_layer  = layer_item.layers
+        for layer in layer_item.layers:
+            if layer.properties.geometryType == "esriGeometryPoint":
+                point_layer = layer
+                break
+        # Query the feature layer to get all the features
+        features = point_layer.query()
+        # Create an empty list to store the coordinates
+        coords = []
+        labels = []
+        Project = []
+        # Loop through the features and extract the coordinates
+        for feature in features:
+            geometry = feature.geometry
+            x = geometry['x']
+            y = geometry['y']
+            coords.append([x, y])
+            attributes = feature.attributes
+            labels.append(attributes['NR'])
+            Project.append(attributes['Opmerking'])
+
+        df = pd.DataFrame(coords, columns=['X', 'Y'])
+        df =df.round(0).astype(int)
+        df['NR'] = labels
+        df['Project'] = Project
+        df.set_index('NR',inplace=True)
+        return df 
+
+    
+    def filter_dataframe(self,pandas,SpecificProject):
+        filtered_df = pandas[pandas['Project'] == SpecificProject]
+        # return(filtered_df)
+        self.Download(filtered_df)
+        
+    def Download(self,pandas):
+        # Define the destination folder path
+        userhome = os.path.expanduser('~')
+        downloads_folder = os.path.join(userhome, 'Downloads')
+        Name_File = "NR.X.Y v1.0 Coördinaten_" + LayerName + ".txt"
+        f = os.path.join(downloads_folder, Name_File)
+        # write the DataFrame to a text file with values separated by a "."
+        # pandas = pandas.drop('Project', axis=1)
+        pandas.to_csv(f, sep='.', index=True)
+        # Open the file for reading
+        with open(f, 'r') as file:
+            # Read the contents of the file
+            contents = file.read()
+            # Replace double quotes with a space
+            contents = contents.replace('"', ' ')
+        # Open the file for writing
+        with open(f, 'w') as file:
+            # Write the modified contents to the file
+            file.write(contents)
+
+#In[]:
---
+++ UI.py
@@ -0,0 +1,112 @@
+#In[]
+
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import *
+from PIL import ImageTk, Image
+from Coordinaten import ArcGIS
+
+#In[]: 
+
+
+window = tk.Tk()
+
+# window.geometry("550x300+300+150")
+# window.resizable(width=True, height=True)
+
+class Vista:
+
+    def __init__(self, window):
+        
+
+        self.Project_Number = tk.StringVar()
+        self.create_widgets()
+
+    def create_widgets(self):
+        
+
+        # Welcome Frame
+
+        welcome_frame = tk.LabelFrame(window, text="Coördinaten uit ArcGIS Online", relief=tk.RIDGE, padx=5, pady=5)
+        welcome_frame.grid(row=1, column=0,pady=7,columnspan = 3)
+        welcome_frame.grid(sticky=tk.E + tk.W + tk.N + tk.S)
+
+        # Imagen Escuela
+
+        image1 = Image.open(f"C:\Python\Coordinaten\Logo\Logo.jpeg")
+        test = ImageTk.PhotoImage(image1)
+        label1 = tk.Label(image=test)
+        label1.grid(row=0, column=0,columnspan = 3)
+        label1.image = test
+
+        # Texto de bievenida
+
+        window.title("Coördinaten uit ArcGIS Online")
+        lbl_welcome = Label(welcome_frame,
+                            text="Hoi! Wat is jouw projectnummer?")
+        lbl_welcome.pack()
+
+        # Frame Buttom search
+        global buttons_frame
+        buttons_frame = tk.LabelFrame(window, text="Project gegevens", relief=tk.RIDGE)
+        buttons_frame.grid(row=2, sticky="nsew",pady = 5)
+
+        # Frame Créditos
+
+        credit_frame = tk.LabelFrame(window,
+                                            text="Contact persoon", relief=tk.RIDGE)
+        credit_frame.grid(row=6, sticky="nsew", pady=5)
+        # Texto de Créditos
+
+        Label(credit_frame,text="Gecrëerd door: MH Poly").grid(row=0, sticky="nsew", pady=2)
+        Label(credit_frame, text="E-mail: aru@mhpoly.com").grid(row=1, sticky="nsew", pady=2)
+        Label(credit_frame, text="Telefoon nummer: +31 (0)164 245 566").grid(row=2, sticky="nsew", pady=2)
+
+        #Coordinates button
+
+        Get_Coordinates_Button = tk.Button(buttons_frame,text = "Raadplegen"
+                                        ,command = self.Coordinaten)
+        Get_Coordinates_Button.grid(row=1,column=0, padx=10,pady=10)
+
+        #Field number of project
+
+        Label(buttons_frame,
+              text="Projectnummer:").grid(row=0, column=0, pady=5, sticky="w")
+
+        ttk.Entry(buttons_frame, width=20, textvariable= self.Project_Number
+                  ).grid(row=1,column=1,sticky=tk.E + tk.W + tk.N + tk.S, pady=5,)
+ 
+
+    def Coordinaten(self):
+            Project = ArcGIS(self.Project_Number.get())
+            Pandas = ArcGIS().get_coordinates(Project.Results())
+            unique_projects = Pandas['Project'].unique().tolist()
+            if len(unique_projects) >= 2:
+                # Create a combobox to select the project
+                global project_combobox
+                project_combobox = ttk.Combobox(buttons_frame, width=20, values=unique_projects
+                  )
+                project_combobox.grid(row=2,column=1,sticky=tk.E + tk.W + tk.N + tk.S, pady=5,)
+                #Create buttom to download the results of an specific project
+                DownloadProject = tk.Button(buttons_frame,text = "Downloaden" 
+                ,command = self.SpecificProjectDownload)
+                DownloadProject.grid(row=2,column=0, padx=0,pady=0)
+            else:
+                Project.Download(Pandas)
+                window.destroy()
+
+    def SpecificProjectDownload(self):          
+            Project = ArcGIS(self.Project_Number.get())
+            Pandas = ArcGIS().get_coordinates(Project.Results())
+            Filtered_Df = ArcGIS().filter_dataframe(Pandas,SpecificProject=project_combobox.get())
+            window.destroy()
+        
+# Create the entire GUI program
+Vista(window)
+
+# Start the GUI event loop
+tk.mainloop()    
+
+#In[]: 
+
