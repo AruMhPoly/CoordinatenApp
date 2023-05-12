e3af5d4f1284d19de23f12d734b22cfbb0ff632a
AruMhPoly <127189008+AruMhPoly@users.noreply.github.com>
2023-05-12 14:02:32 +0200

    Update 12/05/2023

---
+++ ExportSHP.py
@@ -0,0 +1,158 @@
+﻿#In[]
+from arcgis.gis import GIS
+import os 
+import zipfile
+from tkinter import messagebox
+
+#In[]:
+
+class ExportSHP:
+
+    def __init__(self,project_name = "Project Mh Poly"): 
+        #Name of the project
+        self.project_name = project_name
+        self.directory_path = r'P:\2023'
+
+    def CreateFolderToSave(self):
+        
+        #It does not create the general path where everything will be saved. But it looks where it can be saved.
+        Matchingfolder = [name for name in os.listdir(self.directory_path) if 
+                          os.path.isdir(os.path.join(self.directory_path, name)) 
+                          and self.project_name in name]
+        
+        if len(Matchingfolder) != 0:
+            f = os.path.join(self.directory_path, Matchingfolder[0])
+            # Get a list of all folders inside f
+            folders = [name for name in os.listdir(f) if os.path.isdir(os.path.join(f, name))]
+            first_folder = os.path.join(f, folders[0])
+            save_path =  os.path.join(first_folder, "05 Tekeningen", "Werkmap")
+            return save_path
+        else: 
+            save_path = "Geen oveenkomende map gevonden! "
+            return save_path
+    
+    def ArcGISConnection(self):
+        global gis
+        # Connect to your ArcGIS Online organization
+        gis = GIS("https://mhpoly.maps.arcgis.com/", "TSCMHPoly", "n1H1p1*HZEO3")
+
+    def GetNamesLayers(self):
+
+        ##################
+
+        # Search for the feature layer by its name
+        feature_layer_name = self.project_name
+        results = gis.content.search(query=feature_layer_name, item_type="Feature Layer")
+
+        if len(results) != 0: 
+            global feature_layer
+            # x.name is the name of the web feature layer! 
+            Names = [x.name for x in results]
+            for i in range(len(Names)):
+
+                if Names[i] == 'Boringen_HBR':
+                    NP = results[i].id
+                    layer_item = gis.content.get(NP)
+                    feature_layer  = layer_item.layers
+                    Layers_Names = []
+                    for x in feature_layer:
+                        Layers_Names.append(x.properties.name)
+                    return Layers_Names
+                    break
+
+
+
+                elif Names[i] == '22131V1_TE01_WBO_IJmuiden_Uitvoering_WFL1':
+                    NP = results[i].id
+                    layer_item = gis.content.get(NP)
+                    feature_layer  = layer_item.layers
+                    Layers_Names = []
+                    for x in feature_layer:
+                        Layers_Names.append(x.properties.name)
+                    return Layers_Names
+                    break
+            
+
+            NP = results[0].id
+            layer_item = gis.content.get(NP)
+            feature_layer  = layer_item.layers
+            Layers_Names = []
+            for x in feature_layer:
+                Layers_Names.append(x.properties.name)
+            return Layers_Names
+            
+        
+        else:
+
+            message = "Er is geeen project {} gevonden. Probeer het nog eens!".format(self.project_name)
+            # Display the messagebox
+            messagebox.showwarning("Warning", message)
+
+
+        ######################################################################################################
+
+
+    def DownloadLayer(self, NameLayer,save_path):
+        for layer in feature_layer:
+            if layer.properties.name == NameLayer:
+                Layer_To_Download = layer
+                break
+
+        # First, I will see if the folder exists in the P drive
+        if os.path.exists(save_path):
+            # Create a folder to save the SHP file
+            output_folder = os.path.join(save_path, "SHP_" + self.project_name + "_" +
+                                        Layer_To_Download.properties.name)
+            os.makedirs(output_folder, exist_ok=True)
+        else: 
+            userhome = os.path.expanduser('~')
+            downloads_folder = os.path.join(userhome, 'Downloads')
+            output_folder = os.path.join(downloads_folder, "SHP_" + self.project_name + "_" +
+                            Layer_To_Download.properties.name)
+
+
+        # Define the fields to be downloaded
+        fields = ['NR', 'X','Y']
+
+        try: 
+            #For the boringen layer
+            # Query the feature layer and download the results as a SHP file
+            query_result = Layer_To_Download.query(where="1=1", out_fields=["NR,X,Y"], return_geometry=True)
+            query_result.save(out_name = Layer_To_Download.properties.name + ".shp", save_location = output_folder)
+
+        except: 
+            try:
+            #For the vakken layer
+                # Query the feature layer and download the results as a SHP file
+                query_result = Layer_To_Download.query(where="1=1", out_fields=["NR,Shape__Area"], return_geometry=True)
+                query_result.save(out_name = Layer_To_Download.properties.name + ".shp", save_location = output_folder)
+            except: 
+                #For the vakken layer
+                # Query the feature layer and download the results as a SHP file
+                query_result = Layer_To_Download.query(where="1=1", out_fields=["NR,Shape_Area"], return_geometry=True)
+                query_result.save(out_name = Layer_To_Download.properties.name + ".shp", save_location = output_folder)
+
+        # Set the name for the zip file to be created
+        zip_file_name = Layer_To_Download.properties.name + ".zip"
+
+        # Create a ZipFile object with the output file name and mode
+        with zipfile.ZipFile(os.path.join(output_folder, zip_file_name), 'w') as zipf:
+            # Iterate through all files in the directory and add them to the zip file
+            for foldername, subfolders, filenames in os.walk(output_folder):
+                for filename in filenames:
+                    # Ignore the output file itself
+                    if filename != zip_file_name:
+                        file_path = os.path.join(foldername, filename)
+                        zipf.write(file_path, arcname=os.path.relpath(file_path, output_folder))
+
+
+        return output_folder
+#In[]:
+
+# Testen = ExportSHP(project_name="23001")
+# f = Testen.CreateFolderToSave()
+# Testen.ArcGISConnection()
+# Names = Testen.GetNamesLayers()
+# Testen.DownloadLayer(NameLayer=Names[0],save_path=f)
+
+#In[]:
--- UI.py
+++ UI.py
@@ -6,6 +6,8 @@ from tkinter import ttk
 from tkinter.ttk import *
 from PIL import ImageTk, Image
 from Coordinaten import ArcGIS
+from ExportSHP  import ExportSHP
+from tkinter import messagebox
 
 #In[]: 
 
@@ -44,18 +46,64 @@ class Vista:
 
         window.title("Coördinaten uit ArcGIS Online")
         lbl_welcome = Label(welcome_frame,
-                            text="Hoi! Wat is jouw projectnummer?")
+                            text="Hoi! Hoe kan ik je helpen?")
         lbl_welcome.pack()
 
-        # Frame Buttom search
-        global buttons_frame
-        buttons_frame = tk.LabelFrame(window, text="Project gegevens", relief=tk.RIDGE)
-        buttons_frame.grid(row=3, sticky="nsew",pady = 5)
+        # Frame Project Nummer
 
-        #Frame format 
+        ProjectNummer = tk.LabelFrame(window, text="Gegevens", relief=tk.RIDGE)
+        ProjectNummer.grid(row=2, sticky="nsew",pady = 5)        
+        
+        # Text Frame Project Nummer 
+        Label(ProjectNummer,
+        text="Project nummer:           ").grid(row=0, column=0, pady=5, sticky="w")
+        
+        #Entry Frame Project Nummer
 
-        Format_frame = tk.LabelFrame(window, text="Gewenst formaat", relief=tk.RIDGE)
-        Format_frame.grid(row=2, sticky="nsew",pady = 5)
+        ttk.Entry(ProjectNummer, width=20, textvariable= self.Project_Number
+                  ).grid(row=0,column=1,sticky=tk.E + tk.W + tk.N + tk.S, pady=5,)
+
+        #Frame to get the coordinates 
+        global CöordinatenFrame
+        CöordinatenFrame = tk.LabelFrame(window, text="Cöordinaten halen", relief=tk.RIDGE)
+        CöordinatenFrame.grid(row=3, sticky="nsew",pady = 5)
+
+        # First consult and see if there's more than one or more projects
+
+        Label(CöordinatenFrame,
+        text="Gewenst formaat:").grid(row=0, column=0, pady=5, sticky="w")
+
+        Label(CöordinatenFrame,
+        text="Database raadplagen:").grid(row=1, column=0, pady=5, sticky="w")
+
+        #Coordinates button
+
+        Get_Coordinates_Button = tk.Button(CöordinatenFrame,text = "Raadplegen"
+                                        ,command = self.Coordinaten)
+        Get_Coordinates_Button.grid(row=1,column=1, padx=10,pady=10,sticky='nsew')
+
+       # Create a combobox to select the desired format
+        global Format_Combobox
+        Format_Combobox = ttk.Combobox(CöordinatenFrame, width=20, values=["punt scheidingsteken", "komma scheidingsteken","Excel Bestand"]
+            )
+        Format_Combobox.grid(row=0,column=1,sticky=tk.E + tk.W + tk.N + tk.S, pady=5,)
+        Format_Combobox.current(0)
+
+
+        # Frame Download Shapefiles 
+        
+        global SHP_Download_Frame
+        SHP_Download_Frame = tk.LabelFrame(window, text="Shapefile Downloaden", relief=tk.RIDGE)
+        SHP_Download_Frame.grid(row=4, sticky="nsew",pady = 5)
+
+
+        #Buttom to get the layers associated to the project
+
+        GetAvailableLayers = tk.Button(SHP_Download_Frame,text = "Raadplegen"
+                                        ,command = self.ListLayers)
+        GetAvailableLayers.grid(row=0,column=0, padx=10,pady=10,sticky="nsew")
+        # Label(SHP_Download_Frame,
+        # text="Lagen raadplegen").grid(row=0, column=0, pady=5, sticky="w")
 
         # Frame Créditos
 
@@ -63,64 +111,77 @@ class Vista:
                                             text="Contact persoon", relief=tk.RIDGE)
         credit_frame.grid(row=6, sticky="nsew", pady=5)
 
-        #Coordinates button
 
-        Get_Coordinates_Button = tk.Button(buttons_frame,text = "Raadplegen"
-                                        ,command = self.Coordinaten)
-        Get_Coordinates_Button.grid(row=1,column=0, padx=10,pady=10)
-
-        #Field number of project
-
-        Label(buttons_frame,
-              text="Projectnummer:").grid(row=0, column=0, pady=5, sticky="w")
-
-        ttk.Entry(buttons_frame, width=20, textvariable= self.Project_Number
-                  ).grid(row=1,column=1,sticky=tk.E + tk.W + tk.N + tk.S, pady=5,)
-        
-
-        # Create a combobox to select the desired format
-        global Format_Combobox
-        Format_Combobox = ttk.Combobox(Format_frame, width=20, values=["punt scheidingsteken", "komma scheidingsteken","Excel Bestand"]
-            )
-        Format_Combobox.grid(row=0,column=2,sticky=tk.E + tk.W + tk.N + tk.S, pady=5,)
-        Format_Combobox.current(0)
-        Label(Format_frame,text="Formaat:").grid(row=0, column = 1, sticky="nsew", pady=10)
-        
         # Texto de Créditos
 
         Label(credit_frame,text="Gecrëerd door: MH Poly").grid(row=0, sticky="nsew", pady=2)
         Label(credit_frame, text="E-mail: aru@mhpoly.com").grid(row=1, sticky="nsew", pady=2)
         Label(credit_frame, text="Telefoon nummer: +31 (0)164 245 566").grid(row=2, sticky="nsew", pady=2)
- 
+
+       
+    # METHODS FOR THE APPLICATION - UP TO HIER WAS USER INTERFACE
+
+
 
     def Coordinaten(self):
-            Project = ArcGIS(self.Project_Number.get())
-            Pandas = ArcGIS().get_coordinates(Project.Results())
-            try:
-                unique_projects = Pandas['Project'].unique().tolist()
-            except: 
-                #NVT = Niet van Toepassing
-                 unique_projects = ["NVT"]
-            if len(unique_projects) >= 2:
-                # Create a combobox to select the project
-                global project_combobox
-                project_combobox = ttk.Combobox(buttons_frame, width=20, values=unique_projects
-                  )
-                project_combobox.grid(row=2,column=1,sticky=tk.E + tk.W + tk.N + tk.S, pady=5,)
-                #Create buttom to download the results of an specific project
-                DownloadProject = tk.Button(buttons_frame,text = "Downloaden" 
-                ,command = self.SpecificProjectDownload)
-                DownloadProject.grid(row=2,column=0, padx=0,pady=0)
-            else:
-                Project.Download(Pandas,format=Format_Combobox.get())
-                window.destroy()
+        Project = ArcGIS(self.Project_Number.get())
+        global R
+        R = (Project.Results())
+        print(R)
+        Pandas = (Project.get_coordinates(R[2]))
+        try:
+            unique_projects = Pandas['Project'].unique().tolist()
+        except: 
+            #NVT = Niet van Toepassing
+                unique_projects = ["NVT"]
+        if len(unique_projects) >= 2:
+            # Create a combobox to select the project
+            global project_combobox
+            project_combobox = ttk.Combobox(CöordinatenFrame, width=20, values=unique_projects
+                )
+            project_combobox.grid(row=2,column=1,sticky=tk.E + tk.W + tk.N + tk.S, pady=5,)
+            #Create buttom to download the results of an specific project
+            DownloadProject = tk.Button(CöordinatenFrame,text = "Downloaden" 
+            ,command = self.SpecificProjectDownload)
+            DownloadProject.grid(row=2,column=0, padx=0,pady=0)
+        else:
+            Project.Download(Pandas,format=Format_Combobox.get(),ProjectName=R[0],LayerName=R[1])
+            window.destroy()
 
     def SpecificProjectDownload(self):        
-            Project = ArcGIS(self.Project_Number.get())
-            Pandas = ArcGIS().get_coordinates(Project.Results())
-            Filtered_Df = ArcGIS().filter_dataframe(Pandas,SpecificProject=project_combobox.get(),Format_Output=Format_Combobox.get())
-            window.destroy()
-        
+        Project = ArcGIS(self.Project_Number.get())
+        Pandas = Project.get_coordinates(R[2])
+        Filtered_Df = ArcGIS().filter_dataframe(Pandas,SpecificProject=project_combobox.get()
+                                                ,Format_Output=Format_Combobox.get(),
+                                                ProjectName=R[0],LayerName=R[1])
+        window.destroy()
+
+    def ListLayers (self):
+        Project = ExportSHP(project_name=self.Project_Number.get())
+        Project.ArcGISConnection()
+        Names = Project.GetNamesLayers()
+        # Create a combobox to select the project
+        global SHP_Combobox
+        SHP_Combobox = ttk.Combobox(SHP_Download_Frame, width=20, values=Names
+            )
+        SHP_Combobox.grid(row=0,column=1,sticky=tk.E + tk.W + tk.N + tk.S, pady=5,)
+        ##Decoration
+        Label(SHP_Download_Frame,
+              text="    Dat was het!").grid(row=2, column=0, sticky=tk.E + tk.W + tk.N + tk.S, pady=5)
+        #Create buttom to download the results of an specific project
+        DownloadShapeFile = tk.Button(SHP_Download_Frame,text = "Downloaden" 
+        ,command = self.DownloadShapefile)
+        DownloadShapeFile.grid(row=2,column=1, padx=0,pady=0)        
+
+    def DownloadShapefile(self):        
+        Project = ExportSHP(project_name=self.Project_Number.get())
+        f = Project.CreateFolderToSave()
+        OutputFolder = Project.DownloadLayer(NameLayer=SHP_Combobox.get(),save_path=f)
+        message = "De laag werd opgeslagen op: {}".format(OutputFolder)
+        # Display the messagebox
+        messagebox.showwarning("Warning", message)
+        window.destroy()
+
 # Create the entire GUI program
 Vista(window)
 
